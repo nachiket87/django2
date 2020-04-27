49206504a67c8b571e3f80389b1b89fbea914fce
Nachiket Pusalkar <nachiket87@gmail.com>
2020-04-27 06:24:09 -0400

    tried adding image but failed

--- booklog/models.py
+++ booklog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -11,4 +12,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-    
