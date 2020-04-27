27dbb197f78662c01c15153003da336b609e1c95
Nachiket Pusalkar <nachiket87@gmail.com>
2020-04-27 05:10:25 -0400

    added more css and crispy forms

--- booklog/views.py
+++ booklog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Book
 from django.views import generic
 from .forms import BookAddForm
+from django.contrib import messages
 
 
 class Home(generic.ListView):
@@ -17,8 +18,13 @@ class Detail(generic.DetailView):
 def AddBook(request):
     
     form = BookAddForm(request.POST or None)
+
     if form.is_valid():
         form.save()
+        book_title = form.cleaned_data.get('title')
+        book_author = form.cleaned_data.get('author')
+        messages.success(request, f"{book_title} by {book_author} added!")
+        return redirect('books-home')
         
     context = {'form': form}
     return render(request, "booklog/add.html", context)
