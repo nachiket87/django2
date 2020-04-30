cb22514483cfc37e34c6804a1d1130015812ed43
Nachiket Pusalkar <nachiket87@gmail.com>
2020-04-30 04:39:52 -0400

    added user registration with Corey Schafer's help

--- booklog/models.py
+++ booklog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from PIL import Image
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=30, default=' ')
     review_type = models.TextChoices('ReviewType', 'Good Bad Terrible')
     image = models.ImageField(default='default.jpg', upload_to='book_profile')
-    review = models.CharField(blank=True, choices=review_type.choices, max_length=10)
+    review = models.CharField(blank=False, choices=review_type.choices, max_length=10)
     
     
 
