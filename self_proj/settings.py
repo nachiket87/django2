9288b085bb10ec60fde8c22e66679b60b406eea6
Nachiket Pusalkar <nachiket87@gmail.com>
2020-04-30 03:00:38 -0400

    updated 1st part of user registration

--- self_proj/settings.py
+++ self_proj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'booklog.apps.BooklogConfig',
+    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
